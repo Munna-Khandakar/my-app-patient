81ebdabad17287f7ed03ff73c38da47f58730ce3
Munna-Khandakar <munnashisad@gmail.com>
2022-09-01 09:37:13 +0600

    onboarding screen added

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OnboardingScreen from "../screens/OnboardingScreen";
-import HomeScreen from "../screens/HomeScreen";
 import LoginScreen from "../screens/LoginScreen";
-import RegistrationScreen from "../screens/RegistrationScreen";
 import MobileNumberInputScreen from "../screens/Registration/MobileNumberInputScreen";
 import PasswordInputScreen from "../screens/Registration/PasswordInputScreen";
 import OTPInputScreen from "../screens/Registration/OTPInputScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createNativeStackNavigator();
 
 const AuthStack = () => {
+  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+  let routeName;
+
+  useEffect(() => {
+    AsyncStorage.getItem("alreadyLaunched").then((value) => {
+      if (value == null) {
+        AsyncStorage.setItem("alreadyLaunched", "true"); // No need to wait for `setItem` to finish, although you might want to handle errors
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    }); // Add some error handling, also you can simply do setIsFirstLaunch(null)
+  }, []);
+
+  if (isFirstLaunch === null) {
+    return null; // This is the 'tricky' part: The query to AsyncStorage is not finished, but we have to present something to the user. Null will just render nothing, so you can also put a placeholder of some sort, but effectively the interval between the first mount and AsyncStorage retrieving your data won't be noticeable to the user. But if you want to display anything then you can use a LOADER here
+  } else if (isFirstLaunch == true) {
+    routeName = "OnboardingScreen";
+  } else {
+    routeName = "LoginScreen";
+  }
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName={routeName}
+    >
       <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      {/* <Stack.Screen name="RegistrationScreen" component={RegistrationScreen} /> */}
       <Stack.Screen
         name="RegistrationScreen"
         component={MobileNumberInputScreen}
--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,71 +1,46 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  TouchableOpacity,
-  Image,
-} from "react-native";
+import { Image } from "react-native";
 import React from "react";
-import { useFonts } from "expo-font";
-import { MaterialIcons } from "@expo/vector-icons";
+import COLORS from "../utils/Colors";
+import Onboarding from "react-native-onboarding-swiper";
 
 function OnboardingScreen({ navigation }) {
-  const [loaded] = useFonts({
-    Montserrat: require("../../assets/fonts/Montserrat.ttf"),
-  });
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{ marginTop: 50 }}>
-        <Text style={{ fontSize: 30, fontWeight: "bold", color: "#20315f" }}>
-          MYAPP
-        </Text>
-      </View>
-      <Image
-        style={{ height: "40%", width: "90%", resizeMode: "stretch" }}
-        source={require("../../assets/images/misc/gaming.png")}
-      />
-      <TouchableOpacity
-        style={{
-          backgroundColor: "#AD40AF",
-          padding: 20,
-          width: "90%",
-          borderRadius: 5,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: 30,
-        }}
-        onPress={() => navigation.navigate("LoginScreen")}
-      >
-        <Text
-          style={{
-            fontSize: 18,
-            fontWeight: "bold",
-            color: "white",
-            fontFamily: "Montserrat",
-          }}
-        >
-          Let's Begin
-        </Text>
-        <MaterialIcons name="arrow-forward-ios" size={22} color="white" />
-      </TouchableOpacity>
-    </SafeAreaView>
+    <Onboarding
+      onSkip={() => navigation.replace("LoginScreen")}
+      onDone={() => navigation.navigate("LoginScreen")}
+      pages={[
+        {
+          backgroundColor: COLORS.main,
+          image: (
+            <Image
+              source={require("../../assets/onboradimag/LogoTopBan.png")}
+            />
+          ),
+          title: "Doctor Doctor",
+          subtitle: "Meet the most advance patient doctor solution for you..",
+        },
+        {
+          backgroundColor: "#fdeb93",
+          image: (
+            <Image
+              source={require("../../assets/onboradimag/stethoscope.png")}
+            />
+          ),
+          title: "Don't panic during illness",
+          subtitle:
+            "We are here to provide the most efficient software solution for you...",
+        },
+        {
+          backgroundColor: "#e9bcbe",
+          image: (
+            <Image source={require("../../assets/onboradimag/patient.png")} />
+          ),
+          title: "Doctors are available",
+          subtitle: "Call your doctor through the app anywhere anytime",
+        },
+      ]}
+    />
   );
 }
 
 export default OnboardingScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-});
